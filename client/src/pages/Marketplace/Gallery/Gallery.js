edcd078f15bfb3f2a8b39252afe12ad012b90889
Salt Dev <asadimohamadreza@gmail.com>
2021-12-01 12:03:21 +0100

    frontend completed  marketplace page
    Co-authored-by: Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
    Co-authored-by: Jonatan Petersson <petersson.jonatan@gmail.com>

--- client/src/pages/Marketplace/Gallery/Gallery.js
+++ client/src/pages/Marketplace/Gallery/Gallery.js
@@ -3,33 +3,103 @@ import './Gallery.scss'
 import parcel from '../../../assets/img/parcel.png'
 
 const Gallery = () => {
-    return (
-        <section className="gallery" >
-            <ul className="gallery__wrapper">
-                <li className="gallery__wrapper-item" >
-                    <img className="gallery__wrapper-img" src={parcel} alt="parcel" />
-                    <div >
-                        <span>Decentraland</span>
-                        <span className="material-icons-outlined">verified</span>
-                        <span>Price</span>
-                    </div>
-                    <div>
-                        <span>Owner ID</span>
-                        <span>10.000</span>
-                    </div>
-                    <div>
+  return (
+    <section className="gallery" >
+      <ul className="gallery__wrapper">
+        <li className="gallery__wrapper-item" >
+          <img className="gallery__wrapper-img" src={parcel} alt="parcel" />
+          <div className="gallery__wrapper__item" >
+            <div className="gallery__wrapper__item-info" >
+                <div>
+                    <span>Decentraland</span>
+                    <span className="verified-icons-color material-icons-outlined">verified</span>
+                </div>
+                <div>Owner ID</div>
+            </div>
+            <div className="gallery__wrapper__item-info">
+                <div>Price</div>
+                <div>10.000</div>
+            </div>
+          </div>
+          <footer className="gallery__wrapper__footer">
+                <div className="gallery__wrapper__footer-fav">
                     <span className="material-icons-outlined">favorite_border</span>
-                        <span>75</span>
-                    </div>
+                    <span>75</span>
+                </div>
+          </footer>
+        </li>
+        <li className="gallery__wrapper-item" >
+        <img className="gallery__wrapper-img" src={parcel} alt="parcel" />
+          <div className="gallery__wrapper__item" >
+            <div className="gallery__wrapper__item-info" >
+                <div>
+                    <span>Decentraland</span>
+                    <span className="verified-icons-color material-icons-outlined">verified</span>
+                </div>
+                <div>Owner ID</div>
+            </div>
+            <div className="gallery__wrapper__item-info">
+                <div>Price</div>
+                <div>10.000</div>
+            </div>
+          </div>
+          <footer className="gallery__wrapper__footer">
+                <div className="gallery__wrapper__footer-fav">
+                    <span className="material-icons-outlined">favorite_border</span>
+                    <span>75</span>
+                </div>
+          </footer>
+        </li>
+        <li className="gallery__wrapper-item" >
+        <img className="gallery__wrapper-img" src={parcel} alt="parcel" />
+          <div className="gallery__wrapper__item" >
+            <div className="gallery__wrapper__item-info" >
+                <div>
+                    <span>Decentraland</span>
+                    <span className="verified-icons-color material-icons-outlined">verified</span>
+                </div>
+                <div>Owner ID</div>
+            </div>
+            <div className="gallery__wrapper__item-info">
+                <div>Price</div>
+                <div>10.000</div>
+            </div>
+          </div>
+          <footer className="gallery__wrapper__footer">
+                <div className="gallery__wrapper__footer-fav">
+                    <span className="material-icons-outlined">favorite_border</span>
+                    <span>75</span>
+                </div>
+          </footer>
 
-                    </li>
-                <li className="gallery__wrapper-item" >2</li>
-                <li className="gallery__wrapper-item" >3</li>
-                <li className="gallery__wrapper-item" >4</li>
-                <li className="gallery__wrapper-item" >5</li>
-            </ul>
-        </section>
-    )
+        </li>
+        <li className="gallery__wrapper-item" >
+        <img className="gallery__wrapper-img" src={parcel} alt="parcel" />
+          <div className="gallery__wrapper__item" >
+            <div className="gallery__wrapper__item-info" >
+                <div>
+                    <span>Decentraland</span>
+                    <span className="verified-icons-color material-icons-outlined">verified</span>
+                </div>
+                <div>Owner ID</div>
+            </div>
+            <div className="gallery__wrapper__item-info">
+                <div>Price</div>
+                <div>10.000</div>
+            </div>
+          </div>
+          <footer className="gallery__wrapper__footer">
+                <div className="gallery__wrapper__footer-fav">
+                    <span className="material-icons-outlined">favorite_border</span>
+                    <span>75</span>
+                </div>
+          </footer>
+
+        </li>
+
+      </ul>
+    </section>
+  )
 }
 
 export default Gallery
